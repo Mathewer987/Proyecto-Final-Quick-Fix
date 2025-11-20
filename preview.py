2b8bfd422456a7b683d5d4908c0230f23204e5ed
Mathewer987 <mateoaguero31@gmail.com>
2025-11-20 15:00:19 -0300

    preview

---
+++ preview.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def muro():
+    return render_template("muro.html")  # <-- acá ponés el nombre exacto del HTML
+
+if __name__ == "__main__":
+    app.run(debug=True)
